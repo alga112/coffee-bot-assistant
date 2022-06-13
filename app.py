3ed9ae7b4e8391022583701d3de7cb1474addc6d
zaid khan <xzaid60@gmail.com>
2022-06-13 04:55:55 +0300

    chg7

--- app.py
+++ app.py
@@ -10,13 +10,19 @@ def sendPhoto():
     requests.post(f'https://api.telegram.org/bot{TOKEN}/sendPhoto?chat_id={CHAT_ID}&photo={img}')
 
 
-    today = datetime.datetime.now().strftime("%d-%m-%Y")
+    today = datetime.datetime.now().strftime("%d-%m-%Y")# لي ارسال التاريخ مع الصورة كل 6 صباحا 
     text =  'صباح الخير ☕' + '\n' + str(today)
 
-    reply_markup={"keyboard":[["كمية حبوب القهوة ☕️","☕💲شراء بن قهوة"],["➕☕️","كم كوب شربت اليوم؟" ],["/start","🌟supportive push😎"]],"one_time_keyboard":False,"resize_keyboard":True}
+    reply_markup={"keyboard":[ # لصناعة كيبورد 
+                                ["كمية حبوب القهوة ☕️","☕💲شراء بن قهوة"],
+                                ["➕☕️","كم كوب شربت اليوم؟" ],
+                                ["/start","🌟supportive push😎"]],"one_time_keyboard":False,"resize_keyboard":True}
+
     data = {'chat_id': CHAT_ID, 'text': text, 'reply_markup': json.dumps(reply_markup)}
-    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    requests.post(url, data = data)
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"# url الخاص في ال api  الخاص للبوت لكي يرسل له 
+
+    requests.post(url, data = data) # الارسال للبوت 
 
 if __name__ == "__main__":
     sendPhoto()
